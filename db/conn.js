19629f0f41b10db4760ae06fa25fec429007ec68
Leonardo Siqueira <leo2017siqueira@outlook.com>
2023-03-24 17:37:41 -0300

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function main(){
-    await mongoose.connect('mongodb+srv://leonardo:123@node-aula.62yuo64.mongodb.net/PROJNODE?retryWrites=true&w=majority')
+    await mongoose.connect('')
     console.log("Conactado ao Banco!")
 }
 
